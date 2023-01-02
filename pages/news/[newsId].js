f9f30561d0fbda289fcab9dcec80f364446bf2c6
Mayank Vishnoi <mayankvishnoi@Mayanks-MacBook-Air.local>
2023-01-02 19:22:27 +0530

    final commit

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,9 @@
+import { useRouter } from "next/router";
 // our-domain/news/
 
 function NewsDetails() {
+  const router = useRouter();
+  console.log(router.query.newsId);
   return <h1>This is News Details Page</h1>;
 }
 
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+import { Fragment } from "react";
 // our-domain/news
 
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/link1"> Link 1</Link>
+        </li>
+        <li>Link 2</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
